b4ef8b0ed01dadea68e3991eeecc1496f6ffb91b
AidenValley <jhyjsh953@gmail.com>
2022-11-30 18:57:35 -0800

    main app add, catcollector

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('blog/', views.blog, name='blog'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,34 @@
-# main_app/views.py
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# temp add Cats class
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+    def __str__(self):
+        return f"{self.name}"
+
+cats = [
+    Cat('Rufus', 'tabbycat', 'crazy cat', 103),
+    Cat('Simba', 'lion', 'brave', 5),
+    Cat('Garfield', 'tabbycat', 'likes lasagna', 43),
+]
+
+tanner = Cat('tanner', 'tabbycat', 'cool', 23)
+
+# Create your views here.
 def index(request):
-  return HttpResponse('<h1>Hello World! /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return render(request, 'index.html', { 'cats': cats })
 
+def about(request):
+    return render(request, 'about.html', { 'tanner': tanner })
 
+def contact(request):
+    return render(request, 'contact.html')
+
+def blog(request):
+    return render(request, 'blog.html')
